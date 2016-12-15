236998e506120631ea6cb60b528a3d72d423d15b
zhangdesheng <442735829@qq.com>
2016-12-15 17:53:16 +0800

    homePager

--- app/src/main/java/com/dawei/silkroad/dev/HomePagerActivity.java
+++ app/src/main/java/com/dawei/silkroad/dev/HomePagerActivity.java
@@ -35,11 +35,13 @@ import java.util.List;
 
 public class HomePagerActivity extends BaseActivity implements View.OnClickListener {
     private FragmentManager manager;
-    private Fragment[] fragment;
+//    private Fragment[] fragment;
     private HomePagerFragment f1;
     private MineFragment f2;
     private FragmentTransaction transaction;
     private LinearLayout lin_home, lin_mine;
+
+    private Fragment showFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,12 +53,14 @@ public class HomePagerActivity extends BaseActivity implements View.OnClickListe
     private void buildFragmentView() {
         manager = getSupportFragmentManager();
         transaction = manager.beginTransaction();
-        fragment = new Fragment[2];
-        f1 = new HomePagerFragment();
-        fragment[0] = f1;
-        transaction.add(R.id.fg, fragment[0]);
+//        fragment = new Fragment[2];
+//        f1 = new HomePagerFragment();
+//        fragment[0] = f1;
+//        transaction.add(R.id.fg, fragment[0]);
+//        transaction.commit();
+        showFragment = f1;
+        transaction.add(R.id.fg, f1);
         transaction.commit();
-
     }
 
     private void initView() {
@@ -64,6 +68,8 @@ public class HomePagerActivity extends BaseActivity implements View.OnClickListe
         lin_mine = (LinearLayout) findViewById(R.id.lin_mine);
         lin_mine.setOnClickListener(this);
         lin_home.setOnClickListener(this);
+        f1 = new HomePagerFragment();
+        f2 = new MineFragment();
         buildFragmentView();
     }
 
@@ -73,16 +79,27 @@ public class HomePagerActivity extends BaseActivity implements View.OnClickListe
         transaction = manager.beginTransaction();
         switch (view.getId()) {
             case R.id.lin_home:
-                transaction.replace(R.id.fg, fragment[0]);
+                switchContent(showFragment,f1,transaction,manager);
                 break;
             case R.id.lin_mine:
-                if (fragment[1] == null) {
-                    f2 = new MineFragment();
-                    fragment[1] = f2;
-                }
-                transaction.replace(R.id.fg, fragment[1]);
+
+                switchContent(showFragment,f2,transaction,manager);
                 break;
         }
         transaction.commit();
     }
+
+    public void switchContent(Fragment from, Fragment to,FragmentTransaction ft,
+                              FragmentManager fm) {
+        if (from != null) {
+            ft = fm.beginTransaction();
+            if (!to.isAdded()) {    // 先判断是否被add过
+                ft.hide(from).add(R.id.fg, to).commit(); // 隐藏当前的fragment，add下一个到Activity中
+            } else {
+                ft.hide(from).show(to).commit(); // 隐藏当前的fragment，显示下一个
+            }
+        }
+        showFragment = to;
+    }
+
 }
